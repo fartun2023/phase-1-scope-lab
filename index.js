172a6de9748cd0ed1b00d7e25569dbbd41073da3
fartun <fartunwario34@gmail.com>
2023-03-18 11:32:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Declare global variable customerName
+var customerName = "bob";
+const leastFavoriteCustomer = "Dave";
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+    // customerName = customerName.toUpperCase();
+    customerName = customerName.toUpperCase();
+    
+}
+
+// Function to set global variable bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+
+// Function to overwrite global variable bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    bestCustomer = `maybe bob` ;
+}
+
+// // Declare constant leastFavoriteCustomer
+
+// Function to attempt to change constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  // Attempt to assign a new value to leastFavoriteCustomer (which is a constant)
+  leastFavoriteCustomer = "Mike";
+  // This will throw a TypeError: Assignment to constant variable.
+}
